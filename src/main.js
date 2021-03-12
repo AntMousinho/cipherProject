f6ff8f89a379dbc09fddd81986d4174bd95d487a
AntMousinho <antony.mousinho@hotmail.co.uk>
2021-03-12 15:35:40 +0000

    testing npm publish 1.0.2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 const solveCipher = require('./solveCipher');
 
-// let myArgs = process.argv.slice(2);
-
-// solveCipher(myArgs[0], myArgs[1], myArgs[2], myArgs[3]);
-
 const cipher = () => {
     const myArgs = process.argv.slice(2);
     solveCipher(myArgs[0], myArgs[1], myArgs[2], myArgs[3]);
